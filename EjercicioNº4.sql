37cfd7ab3276c6f3c71e57c8378e7395a29f17e3
agusdiisanto <agusdisanto99@gmail.com>
2022-10-04 18:25:09 -0300

    TP-SQL

--- EjercicioNº4.sql
+++ EjercicioNº4.sql
@@ -16,3 +16,25 @@ ON comercio.comercio = precio.comercio
 JOIN producto 
 ON producto.pid = precio.pid
 WHERE producto.descripcion = 'Frutas Secas' and precio.precio < 40
+
+-- (d) Productos Exclusivos: Listar los <pid, descripcion> de los productos que son vendidos
+en el barrio de Palermo, pero que no son vendidos en el barrio de San Telmo.
+--
+
+SELECT p.pid, p.descripcion 
+FROM producto AS p
+JOIN precio 
+ON p.pid = precio.pid 
+JOIN comercio
+ON precio.comercio = comercio.comercio
+WHERE comercio.barrio = 'Palermo' 
+
+EXCEPT 
+
+SELECT p.pid, p.descripcion 
+FROM producto AS p
+JOIN precio 
+ON p.pid = precio.pid 
+JOIN comercio
+ON precio.comercio = comercio.comercio
+WHERE comercio.barrio = 'San Telmo' 
