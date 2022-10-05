dee0a84487472dbc0f705bedb1b6f2481ac4d482
agusdiisanto <agusdisanto99@gmail.com>
2022-10-05 17:07:14 -0300

    TP

--- EjercicioNº4.sql
+++ EjercicioNº4.sql
@@ -18,8 +18,8 @@ ON producto.pid = precio.pid
 WHERE producto.descripcion = 'Frutas Secas' and precio.precio < 40
 
 -- (d) Productos Exclusivos: Listar los <pid, descripcion> de los productos que son vendidos
-en el barrio de Palermo, pero que no son vendidos en el barrio de San Telmo.
---
+-- en el barrio de Palermo, pero que no son vendidos en el barrio de San Telmo.
+
 
 SELECT p.pid, p.descripcion 
 FROM producto AS p
@@ -38,3 +38,14 @@ ON p.pid = precio.pid
 JOIN comercio
 ON precio.comercio = comercio.comercio
 WHERE comercio.barrio = 'San Telmo' 
+
+--(e) Barrios: Listar el <precio-promedio, barrio> del precio promedio de los productos
+--ofrecidos en cada barrio.
+
+SELECT barrio, avg(precio) AS precio_promedio
+FROM comercio
+JOIN precio
+ON comercio.comercio = precio.comercio
+GROUP BY barrio
+
+-- (f) Los precios de ahorita: Obtener la lista de los productos con sus precios actuales.
